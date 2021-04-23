2a751e6095ad12292309150166997973bc7907dd
anilburakbilsel <abbilsel@gmail.com>
2021-04-22 20:35:49 -0700

    type checking is added for nodes

--- JavaScript/List/Node.js
+++ JavaScript/List/Node.js
@@ -1,8 +1,16 @@
 // a node in list
 export default class Node {
     constructor(data, next = null) {
+        if ((typeof data) !== Number) {
+            throw 'the data has to be a number value!';
+        }
         this.data = data;
-        this.next = next;
+        if ((typeof data) === Node || next === null) {
+            this.next = next;
+        }
+        else {
+            throw 'it can either point to null or another Node!';
+        }
     }
 
     toString(callback) {
