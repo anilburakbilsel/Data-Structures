dcbdcd70cfab04c06fc7b7dab5f09bde75b4e176
anilburakbilsel <abbilsel@gmail.com>
2021-04-25 14:04:27 -0700

    Comparator.js file is implemented

--- JavaScript/Utils/Comparator.js
+++ JavaScript/Utils/Comparator.js
@@ -1,8 +1,8 @@
 export default class Comparator {
   /**
-   * if a comparafunction is provided, we will use it
-   * otherwise use the default one
-   * @updates the compare value
+   * if a compare function is provided, we will use it
+   * otherwise we use the default one
+   * @update the compare value
    */
   constructor(customCompareFunction) {
     this.compare = customCompareFunction || Comparator.defaultCompareFunction;
@@ -11,12 +11,61 @@ export default class Comparator {
   /**
    * the default comparison function
    * the compared values can be string or number
-   * @returns 0, -1 or 1
+   * the order is from a to b
+   * @return {0, -1 or 1}
    */
   static defaultCompareFunction(a, b) {
     if (a === b) {
       return 0;
     }
     return a < b ? -1 : 1;
+  }
+
+  /**
+   * checks if two variables are equal
+   * @return {boolean}
+   */
+  equal(a, b) {
+    return this.compare(a, b) === 0;
+  }
+
+  /**
+   * checks if variable "a" is less than "b"
+   * @return {boolean}
+   */
+   lessThan(a, b) {
+    return this.compare(a, b) < 0;
+  }
+
+  /**
+   * checks if variable "a" is greater than "b"
+   * @return {boolean}
+   */
+  greaterThan(a, b) {
+    return this.compare(a, b) > 0;
+  }
+
+  /**
+   * checks if variable "a" is less than or equal to "b"
+   * @return {boolean}
+   */
+  lessThanOrEqual(a, b) {
+    return this.lessThan(a, b) || this.equal(a, b);
+  }
+
+  /**
+   * checks if variable "a" is greater than or equal to "b"
+   * @return {boolean}
+   */
+  greaterThanOrEqual(a, b) {
+    return this.greaterThan(a, b) || this.equal(a, b);
+  }
+
+  /**
+   * this reverses the comparison order
+   */
+  reverse() {
+    const compareOriginal = this.compare;
+    this.compare = (a, b) => compareOriginal(b, a);
   }
 }
